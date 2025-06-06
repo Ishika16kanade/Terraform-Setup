7836a4491a09aae94d435a042fad3a545e2ef650
Ishika Kanade <79101535+Ishika16kanade@users.noreply.github.com>
2025-06-06 15:42:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "vm" {
   ami           = "ami-08355844f8bc94f55"  # Make sure this AMI exists in your region
   instance_type = "t2.medium"
-  key_name      = "laptop-key"             # Ensure this key pair exists in your AWS account
+  key_name      = "S_Key1"             # Ensure this key pair exists in your AWS account
 
   tags = {
     Name = "TF-SERVER"
